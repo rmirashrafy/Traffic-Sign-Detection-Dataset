443e58b7b623a5eeb7a9a126cd59f3d880e70409
F.M <fatemehmira394@gmail.com>
2026-05-06 00:04:31 +0330

    Update TrainExample.py

--- TrainExample.py
+++ TrainExample.py
@@ -1,14 +1,9 @@
-from ultralytics import YOLO
+
+!pip install roboflow
+
 from roboflow import Roboflow
 
-model = YOLO("yolov8n.pt")
-
-model.train(
-    data="data.yaml",
-    epochs=50,
-    imgsz=640
-)
-
 rf = Roboflow(api_key="YOUR_API_KEY")
-project = rf.workspace("workspace-name").project("project-name")
-dataset = project.version(1).download("yolov8")
+project = rf.workspace("yolo-self-driving-car").project("crosswalk-sign-recognition")
+version = project.version(3)
+dataset = version.download("yolov8")
